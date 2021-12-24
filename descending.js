fc74aec3cc027723804bdcc5d09db1c83cc3fed3
Oleg <89839445+traufholz@users.noreply.github.com>
2021-12-24 14:41:50 +0300

    Add files via upload

---
+++ descending.js
@@ -0,0 +1,3 @@
+const num = [1,6,7,8,3,4,5,6]
+num.sort((a,b) => b - a);
+console.log(num)
---
+++ numbers.js
@@ -0,0 +1,3 @@
+const numbers = [1,6,7,8,3,4,5,6];
+let arr = numbers.filter(item => item > 3);
+console.log(arr);
